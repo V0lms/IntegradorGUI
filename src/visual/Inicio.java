336a46ff0f8de407bf67253d9b461a80ba26c718
V0lms <victorolmosmartin@gmail.com>
2024-05-17 11:08:30 +0200

    Arreglar btn iniciar sesion

--- src/visual/Inicio.java
+++ src/visual/Inicio.java
@@ -36,13 +36,13 @@ public class Inicio extends JFrame {
 				try {
 					Inicio frame = new Inicio();
 					frame.setVisible(true);
-					
+
 					if(iniciarSesion==true) {
 						frame.setVisible(false);
 					}else {
 						//apagar iniciar sesion
 					}
-					
+
 				} catch (Exception e) {
 					System.out.println(e.getMessage()+"Error al crear el frame de bienvenida");
 				}
@@ -66,35 +66,35 @@ public class Inicio extends JFrame {
 		pnlMain.setLayout(null);
 
 		//------------------------------------------------------------
-		
+
 		WELCOME = new JPanel();
 		WELCOME.setBackground(new Color(255, 255, 255));
 		WELCOME.setBounds(0, 0, 569, 391);
 		pnlMain.add(WELCOME);
 		WELCOME.setLayout(null);
-		
+
 		LOGIN = new JPanel();
 		LOGIN.setLayout(null);
 		LOGIN.setBackground(Color.WHITE);
 		LOGIN.setBounds(0, 0, 569, 391);
 		pnlMain.add(LOGIN);
 		LOGIN.setVisible(false);
-		
-		
+
+
 		//------------------------------------------------------------
-		
+
 		txtpnSuperempresa_1 = new JTextPane();
 		txtpnSuperempresa_1.setText("SUPEREMPRESA");
 		txtpnSuperempresa_1.setForeground(Color.RED);
 		txtpnSuperempresa_1.setFont(new Font("DejaVu Sans", Font.BOLD, 30));
 		txtpnSuperempresa_1.setBounds(28, 28, 294, 42);
 		LOGIN.add(txtpnSuperempresa_1);
-		
+
 		txtNameUsuario = new JTextField();
 		txtNameUsuario.setBounds(28, 110, 140, 23);
 		LOGIN.add(txtNameUsuario);
 		txtNameUsuario.setColumns(10);
-		
+
 		txtpnUsuario = new JTextPane();
 		txtpnUsuario.setForeground(new Color(153, 0, 51));
 		txtpnUsuario.setFont(new Font("DejaVu Sans", Font.BOLD, 14));
@@ -106,14 +106,14 @@ public class Inicio extends JFrame {
 			}
 		});
 		LOGIN.add(txtpnUsuario);
-		
+
 		txtpnContrasenya = new JTextPane();
 		txtpnContrasenya.setText("Contraseña");
 		txtpnContrasenya.setForeground(new Color(153, 0, 51));
 		txtpnContrasenya.setFont(new Font("DejaVu Sans", Font.BOLD, 14));
 		txtpnContrasenya.setBounds(28, 155, 140, 23);
 		LOGIN.add(txtpnContrasenya);
-		
+
 		passwContrasenya = new JPasswordField();
 		passwContrasenya.setBounds(28, 180, 140, 21);
 		passwContrasenya.addKeyListener(new KeyAdapter() {
@@ -122,18 +122,19 @@ public class Inicio extends JFrame {
 			}
 		});
 		LOGIN.add(passwContrasenya);
-		
+
 		btnIniciarSesion = new JButton("INICIAR SESION");
+		btnIniciarSesion.setEnabled(false);
 		btnIniciarSesion.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				
+
 			}
 		});
 		btnIniciarSesion.setBackground(new Color(204, 0, 51));
 		btnIniciarSesion.setForeground(new Color(255, 255, 255));
 		btnIniciarSesion.setBounds(28, 219, 140, 49);
 		LOGIN.add(btnIniciarSesion);
-		
+
 		btnVolver = new JButton("VOLVER");
 		btnVolver.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -143,32 +144,32 @@ public class Inicio extends JFrame {
 		});
 		btnVolver.setBounds(28, 280, 140, 27);
 		LOGIN.add(btnVolver);
-		
+
 		//PANELES-----------------------------------------------------
-		
-		
+
+
 		btnUsuario = new JButton("INICIAR COMO GEST");
 		btnUsuario.setBounds(51, 162, 192, 42);
 		WELCOME.add(btnUsuario);
-		
+
 		btnInvitado = new JButton("INICIAR SESION");
 		btnInvitado.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				LOGIN.setVisible(true);
 				WELCOME.setVisible(false);
-				
+
 			}
 		});
 		btnInvitado.setBounds(51, 108, 192, 42);
 		WELCOME.add(btnInvitado);
-		
+
 		txtpnSuperempresa = new JTextPane();
 		txtpnSuperempresa.setBounds(28, 28, 294, 42);
 		WELCOME.add(txtpnSuperempresa);
 		txtpnSuperempresa.setFont(new Font("DejaVu Sans", Font.BOLD, 30));
 		txtpnSuperempresa.setForeground(new Color(255, 0, 0));
 		txtpnSuperempresa.setText("SUPEREMPRESA");
-		
+
 		btnPreguntasFrecuentes = new JButton("PREGUNTAS FRECUENTES");
 		btnPreguntasFrecuentes.setBounds(51, 218, 192, 42);
 		WELCOME.add(btnPreguntasFrecuentes);
@@ -176,8 +177,8 @@ public class Inicio extends JFrame {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		
-		
+
+
 
 	}
 
@@ -190,7 +191,7 @@ public class Inicio extends JFrame {
 			}
 		} catch (Exception e) {
 			System.out.println(e.getMessage()+"No se puede actualizar el boton");
-		}	
+		}
 	}
-	
+
 }
